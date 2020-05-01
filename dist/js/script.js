ae47a3f16e2c7933bcfd43c2521eaa0c563378dd
Ania Dalecka <gapiontko@o2.pl>
2020-05-01 17:58:56 +0200

    Create class Cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -336,6 +336,27 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.ptoducts = [];
+
+      thisCart.getElements (element);
+
+      console.log('new cart:', thisCart);
+
+    }
+
+    getElements (element){
+      const thisCart = this;
+
+      thisCart.dom ={};
+
+      this.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
