162596bdaaca05cb1a42c31ab9c1b735362125f5
Ania Dalecka <gapiontko@o2.pl>
2020-05-04 14:30:30 +0200

    Add json server

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,13 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+
+    //Use API
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
@@ -557,14 +564,32 @@
       //console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch (url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+          thisApp.initMenu();
+
+        });
+
     },
 
     init: function(){
@@ -576,8 +601,6 @@
       console.log('templates:', templates);
 
       thisApp.initData();
-
-      thisApp.initMenu();
 
       thisApp.initCart();
     },
